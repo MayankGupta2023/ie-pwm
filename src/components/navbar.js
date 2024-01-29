e9606e7d9b0fe1dd02c9c45df56f73fd03ac4b20
Mayank Gupta <mayankgupta20231@gmail.com>
2024-01-29 23:02:04 +0530

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '@/assets/logo.png';
+import styles from '@/styles/navbar.module.css';
+
+// console.log(logo);   
+
+const Navbar = () => {
+    return (
+        <div className={`h-full flex justify-between px-4 items-center text-xl ${styles.nav}`}>
+            <div>
+                <img src={logo.src} alt='logo' className={`h-12 ${styles.logo}`} />
+            </div>
+
+            <div className='flex justify-around min-w-96 p-2 '>
+                <div className='hover:cursor-pointer'>Pricing</div>
+                <div className='hover:cursor-pointer'>Login</div>
+                <div className='hover:cursor-pointer'>Notes</div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Navbar
