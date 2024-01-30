9a23110d1b21a340cb184f1e38c70b0297ea1e3a
Rohit Tripathi <rt264@snu.edu.in>
2024-01-30 16:32:43 +0530

    cleaned ui of login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,237 @@
+// import { useState, useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import { motion } from 'framer-motion';
+// import firebase from 'firebase/app';
+// import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+// import app from '../firebaseConfig';
+// import { getFirestore, doc, setDoc } from 'firebase/firestore';
+
+// const auth = getAuth(app);
+
+// const AuthPage = () => {
+//   const router = useRouter();
+
+//   const [isLogin, setIsLogin] = useState(true);
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [name, setName] = useState('');
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     const unsubscribe = auth.onAuthStateChanged((user) => {
+//       if (user) {
+//         router.push('/dashboard');
+//       }
+//     });
+
+//     return () => unsubscribe();
+//   }, [router]);
+
+//   const handleAuth = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       setLoading(true);
+
+//       if (isLogin) {
+//         // Login
+//         await signInWithEmailAndPassword(auth, email, password);
+//         router.push('/dashboard');
+//       } else {
+//         // Sign Up
+//         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+//         const user = userCredential.user;
+
+//         const db = getFirestore(app);
+//         await setDoc(doc(db, 'users', user.uid), {
+//           name,
+//           email,
+//         });
+
+//         router.push('/dashboard');
+//       }
+//     } catch (error) {
+//       console.error(error.message);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+//       <motion.div
+//         initial={{ opacity: 0, y: -20 }}
+//         animate={{ opacity: 1, y: 0 }}
+//         exit={{ opacity: 0, y: -20 }}
+//         transition={{ duration: 0.5, delay: 0.2 }}
+//         className="bg-white bg-opacity-90 p-10 rounded-md shadow-md max-w-md w-full flex flex-col items-center"
+//       >
+//         <motion.div className="flex mb-6">
+//           <motion.div
+//             whileHover={{ scale: 1.1 }}
+//             whileTap={{ scale: 0.9 }}
+//             className={`cursor-pointer text-2xl mx-2 ${
+//               isLogin ? 'text-blue-600 font-semibold' : 'text-gray-600'
+//             }`}
+//             onClick={() => setIsLogin(true)}
+//           >
+//             Login
+//           </motion.div>
+//           <motion.div
+//             whileHover={{ scale: 1.1 }}
+//             whileTap={{ scale: 0.9 }}
+//             className={`cursor-pointer text-2xl mx-2 ${
+//               !isLogin ? 'text-blue-600 font-semibold' : 'text-gray-600'
+//             }`}
+//             onClick={() => setIsLogin(false)}
+//           >
+//             Sign Up
+//           </motion.div>
+//         </motion.div>
+
+//         <motion.h2
+//           initial={{ opacity: 0, y: -20 }}
+//           animate={{ opacity: 1, y: 0 }}
+//           transition={{ duration: 0.5 }}
+//           className="text-3xl font-semibold mb-6 text-gray-800"
+//         >
+//           {isLogin ? 'Login' : 'Sign Up'}
+//         </motion.h2>
+//         <motion.form
+//           initial={{ opacity: 0, y: 20 }}
+//           animate={{ opacity: 1, y: 0 }}
+//           transition={{ duration: 0.5 }}
+//           onSubmit={(e) => {
+//             e.preventDefault();
+//             handleAuth(e);
+//           }}
+//         >
+//           {isLogin ? (
+//             <>
+//               <div className="mb-6">
+//                 <label htmlFor="email" className="block text-gray-600 mb-2">
+//                   Email
+//                 </label>
+//                 <input
+//                   type="email"
+//                   id="email"
+//                   name="email"
+//                   value={email}
+//                   onChange={(e) => setEmail(e.target.value)}
+//                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+//                   placeholder="Enter your email"
+//                 />
+//               </div>
+//               <div className="mb-6">
+//                 <label htmlFor="password" className="block text-gray-600 mb-2">
+//                   Password
+//                 </label>
+//                 <input
+//                   type="password"
+//                   id="password"
+//                   name="password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+//                   placeholder="Enter your password"
+//                 />
+//               </div>
+//               <div className="flex items-center justify-between">
+//                 <motion.button
+//                   type="button"
+//                   whileHover={{ scale: 1.1 }}
+//                   whileTap={{ scale: 0.9 }}
+//                   className="text-blue-600 hover:underline cursor-pointer"
+//                   onClick={() => setIsLogin(true)}
+//                 >
+//                   Forgot Password?
+//                 </motion.button>
+//                 <motion.button
+//                   type="submit"
+//                   whileHover={{ scale: 1.1, x: 5 }}
+//                   whileTap={{ scale: 0.9 }}
+//                   className="bg-blue-600 text-white px-8 py-4 rounded-full hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300"
+//                   disabled={loading}
+//                 >
+//                   {loading ? 'Loading...' : 'Login'}
+//                 </motion.button>
+//               </div>
+//             </>
+//           ) : (
+//             <>
+//               <div className="mb-6">
+//                 <label htmlFor="name" className="block text-gray-600 mb-2">
+//                   Name
+//                 </label>
+//                 <input
+//                   type="text"
+//                   id="name"
+//                   name="name"
+//                   value={name}
+//                   onChange={(e) => setName(e.target.value)}
+//                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+//                   placeholder="Enter your name"
+//                 />
+//               </div>
+//               <div className="mb-6">
+//                 <label htmlFor="email" className="block text-gray-600 mb-2">
+//                   Email
+//                 </label>
+//                 <input
+//                   type="email"
+//                   id="email"
+//                   name="email"
+//                   value={email}
+//                   onChange={(e) => setEmail(e.target.value)}
+//                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+//                   placeholder="Enter your email"
+//                 />
+//               </div>
+//               <div className="mb-6">
+//                 <label htmlFor="password" className="block text-gray-600 mb-2">
+//                   Password
+//                 </label>
+//                 <input
+//                   type="password"
+//                   id="password"
+//                   name="password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+//                   placeholder="Enter your password"
+//                 />
+//               </div>
+//               <div className="flex items-center justify-between">
+//                 <motion.button
+//                   type="button"
+//                   whileHover={{ scale: 1.1 }}
+//                   whileTap={{ scale: 0.9 }}
+//                   className="text-blue-600 hover:underline cursor-pointer "
+//                   onClick={() => setIsLogin(true)}
+//                 >
+//                   Already have an account? Login
+//                 </motion.button>
+//                 <motion.button
+//                   type="submit"
+//                   whileHover={{ scale: 1.1, x: 5 }}
+//                   whileTap={{ scale: 0.9 }}
+//                   className="bg-purple-600 text-white px-8 py-4 rounded-full hover:bg-purple-700 focus:outline-none focus:ring focus:border-blue-300"
+//                   disabled={loading}
+//                 >
+//                   {loading ? 'Loading...' : 'Sign Up'}
+//                 </motion.button>
+//               </div>
+//             </>
+//           )}
+//         </motion.form>
+//       </motion.div>
+//     </div>
+//   );
+// };
+
+// export default AuthPage;
+
+
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { motion } from 'framer-motion';
@@ -15,9 +249,9 @@ const AuthPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // Redirect to dashboard if the user is already logged in
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         router.push('/dashboard');
@@ -31,6 +265,8 @@ const AuthPage = () => {
     e.preventDefault();
 
     try {
+      setLoading(true);
+
       if (isLogin) {
         // Login
         await signInWithEmailAndPassword(auth, email, password);
@@ -40,7 +276,6 @@ const AuthPage = () => {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
 
-        // Store user details in Firestore collection "users"
         const db = getFirestore(app);
         await setDoc(doc(db, 'users', user.uid), {
           name,
@@ -51,6 +286,8 @@ const AuthPage = () => {
       }
     } catch (error) {
       console.error(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -100,7 +337,7 @@ const AuthPage = () => {
           transition={{ duration: 0.5 }}
           onSubmit={(e) => {
             e.preventDefault();
-            handleAuth(e); // Pass event to handleAuth function
+            handleAuth(e);
           }}
         >
           {isLogin ? (
@@ -113,8 +350,8 @@ const AuthPage = () => {
                   type="email"
                   id="email"
                   name="email"
-                  value={email} // Bind value to state
-                  onChange={(e) => setEmail(e.target.value)} // Handle input change
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
                   placeholder="Enter your email"
                 />
@@ -127,8 +364,8 @@ const AuthPage = () => {
                   type="password"
                   id="password"
                   name="password"
-                  value={password} // Bind value to state
-                  onChange={(e) => setPassword(e.target.value)} // Handle input change
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
                   placeholder="Enter your password"
                 />
@@ -138,7 +375,7 @@ const AuthPage = () => {
                   type="button"
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.9 }}
-                  className="text-blue-600 hover:underline cursor-pointer"
+                  className="text-blue-600 hover:underline cursor-pointer ml-0"
                   onClick={() => setIsLogin(true)}
                 >
                   Forgot Password?
@@ -147,9 +384,10 @@ const AuthPage = () => {
                   type="submit"
                   whileHover={{ scale: 1.1, x: 5 }}
                   whileTap={{ scale: 0.9 }}
-                  className="bg-blue-600 text-white px-8 py-4 rounded-full hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300"
+                  className="bg-blue-600 text-white px-8 py-4 rounded-full hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300 ml-10"
+                  disabled={loading}
                 >
-                  Login
+                  {loading ? 'Loading...' : 'Login'}
                 </motion.button>
               </div>
             </>
@@ -163,8 +401,8 @@ const AuthPage = () => {
                   type="text"
                   id="name"
                   name="name"
-                  value={name} // Bind value to state
-                  onChange={(e) => setName(e.target.value)} // Handle input change
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
                   placeholder="Enter your name"
                 />
@@ -177,8 +415,8 @@ const AuthPage = () => {
                   type="email"
                   id="email"
                   name="email"
-                  value={email} // Bind value to state
-                  onChange={(e) => setEmail(e.target.value)} // Handle input change
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
                   placeholder="Enter your email"
                 />
@@ -191,8 +429,8 @@ const AuthPage = () => {
                   type="password"
                   id="password"
                   name="password"
-                  value={password} // Bind value to state
-                  onChange={(e) => setPassword(e.target.value)} // Handle input change
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   className="w-full px-4 py-3 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
                   placeholder="Enter your password"
                 />
@@ -202,7 +440,7 @@ const AuthPage = () => {
                   type="button"
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.9 }}
-                  className="text-blue-600 hover:underline cursor-pointer "
+                  className="text-blue-600 hover:underline cursor-pointer ml-0"
                   onClick={() => setIsLogin(true)}
                 >
                   Already have an account? Login
@@ -211,9 +449,10 @@ const AuthPage = () => {
                   type="submit"
                   whileHover={{ scale: 1.1, x: 5 }}
                   whileTap={{ scale: 0.9 }}
-                  className="bg-purple-600 text-white px-8 py-4 rounded-full hover:bg-purple-700 focus:outline-none focus:ring focus:border-blue-300"
+                  className="bg-purple-600 text-white px-8 py-4 rounded-full hover:bg-purple-700 focus:outline-none focus:ring focus:border-blue-300 ml-8" 
+                  disabled={loading}
                 >
-                  Sign Up
+                  {loading ? 'Loading...' : 'Sign Up'}
                 </motion.button>
               </div>
             </>
@@ -225,3 +464,4 @@ const AuthPage = () => {
 };
 
 export default AuthPage;
+
