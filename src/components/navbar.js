a56e449ae056ac98abd9add84eab7cfda81be4a5
Aayush <aayush12arora@gmail.com>
2024-01-31 20:08:12 +0530

    Refactor navbar component and update styles

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo  from '../assets/logo.png';
-import styles from '../styles/navbar.module.css';
+// Navbar.js
+import React, { useState, useEffect } from 'react';
+import { getAuth, signOut } from 'firebase/auth';
 import Link from 'next/link';
-
+import logo from '../assets/logo.png';
+import styles from '../styles/landing.module.css';
+import app from '../firebaseConfig';
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const auth = getAuth();
+  const auth = getAuth(app);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -27,24 +29,10 @@ const Navbar = () => {
   return (
     <div className={`h-full flex justify-between px-4 items-center text-xl rounded-l`}>
       <div className='flex text-black gap-4 items-center text-3xl'>
-        {/* Your logo and site name */}
+      <img src={logo.src} alt='logo' className={`h-12 ${styles.logo}`} />
         Instant EduDoc
       </div>
 
-            <div className='flex justify-around min-w-96 p-2 '>
-
-
-                <Link href="/">Home</Link>
-                <Link href="/pricing">Pricing</Link>
-                <Link href="/Working">Get Notes</Link>
-                <Link href="/login">Login</Link>
-
-            </div>
-
-
-        </div>
-    )
-}
       <div className='flex justify-around min-w-96 p-2 '>
         <Link href="/">Home</Link>
         <Link href="/pricing">Pricing</Link>
