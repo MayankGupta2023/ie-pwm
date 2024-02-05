69904ef3aaeef19ed19d51290692e54827937100
Mayank Gupta <mayankgupta20231@gmail.com>
2024-02-05 23:30:27 +0530

    Add plus icon to FAQ items

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import styles from "../styles/landing.module.css"
 import Footer from "../components/footer";
 import { motion, useAnimation } from "framer-motion";
+import plus from "../assets/plus.svg"
 
 
 export default function Home() {
@@ -321,20 +322,20 @@ export default function Home() {
         <motion.div
           className="max-h-1/2 w-3/5 bg-stone-100 rounded-2xl ">
           <div className="h-fit p-4 text-lg font-semibold flex justify-center flex-col ">
-            <div style={{ color: '#333333' }} className="h-fit" onClick={() => { handleDrop1() }}>+ What is Framer?</div>
+            <div style={{ color: '#333333' }} className="h-fit flex gap-2" onClick={() => { handleDrop1() }}> <img src={plus.src} />  What is Framer?</div>
             {drop1 ? <motion.div style={{ color: '#666666' }} className="h-fit px-4 text-lg font-semibold flex items-center">
               Framer is web builder for creative pros. Be sure to check out framer.com to learn more.
             </motion.div> : null}
           </div>
           <div className="h-fit  p-4 text-lg font-semibold flex justify-center flex-col">
-            <div style={{ color: '#333333' }} className="h-fit" onClick={() => { handleDrop2() }}>+ Is it easy to learn?</div>
+            <div style={{ color: '#333333' }} className="h-fit flex gap-2" onClick={() => { handleDrop2() }}> <img src={plus.src} />  Is it easy to learn?</div>
             {drop2 ? <motion.div style={{ color: '#666666' }}
               className="h-fit px-4 text-lg font-semibold flex items-center">
               Framer is fastest tool to build sites with, because you can ship your design immidiatly,insted of rebuilding it in code or second tool.
             </motion.div> : null}
           </div>
           <div style={{ color: '#333333' }} className="h-fit  p-4 text-lg font-semibold flex justify-center flex-col">
-            <div className="h-fit" onClick={() => { handleDrop3() }}>+ Do i need to code?</div>
+            <div className="h-fit flex gap-2" onClick={() => { handleDrop3() }}> <img src={plus.src} />  Do i need to code?</div>
             {drop3 ? <motion.div style={{ color: '#666666' }}
 
               className="h-fit px-4 text-lg font-semibold flex items-center">
