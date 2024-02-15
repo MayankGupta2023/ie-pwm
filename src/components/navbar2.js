62a3f2cf78514b940cb896a6381d55ae417e762e
Aayush <aayush12arora@gmail.com>
2024-02-15 20:15:05 +0530

    Update package-lock.json and navbar.js

--- src/components/navbar2.js
+++ src/components/navbar2.js
@@ -1,19 +1,17 @@
 // Navbar.js
 import React, { useState, useEffect } from 'react';
 import { getAuth, signOut } from 'firebase/auth';
-import { useRouter } from 'next/router';
 import Link from 'next/link';
 import logo from '../assets/logo.png';
 import styles from '../styles/landing.module.css';
 import app from '../firebaseConfig';
-import menu from '../assets/menu.svg';
-import cross from '../assets/cross2.svg';
+import menu from '../assets/menu.svg'
+import cross from '../assets/cross2.svg'
 
 const Navbar = () => {
   const [bars, setBars] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const auth = getAuth(app);
-  const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -32,27 +30,21 @@ const Navbar = () => {
     }
   };
 
-  const handleLogoClick = () => {
-    router.push('/');
-  };
-
-  const handleClick = () => {
-    setBars(!bars);
-  };
-
   return (
     <div style={{ color: 'rgb(102, 102, 102)' }} className={`h-full bg-white flex justify-between tracking-tight pl-4 pr-2 items-center font-medium  rounded-l font-inter ${styles.b1}`}>
-      <a href='/' onClick={handleLogoClick}>
-        <div className='flex text-black gap-4 items-center text-3xl'>
-          <img src={logo.src} alt='logo' className={`h-12 ${styles.logo}`} />
-        </div>
-      </a>
+        <a href='/' >
+  <div className='flex text-black gap-4 items-center text-3xl'>
+    <img src={logo.src} alt='logo' className={`h-12 ${styles.logo}`} />
+  </div>
+</a>
 
-      <div className={`text-base flex justify-around min-w-80 ${styles.nav} ${bars ? '' : styles.navhide}`} onClick={handleClick}>
-        <Link href="/">Home</Link>
-        <Link href="/pricing">Upgrade</Link>
-        <Link href="/pricing">Contact</Link>
-        <Link href="/Working">Profile</Link>
+      <div className={bars ? 'hidden' : `text-base flex  justify-around min-w-80 ${styles.nav}`}>
+        <Link href="/">Upgrade</Link>
+       
+     <Link href="/pricing">Contact</Link>
+
+        <Link href="/profile">Profile</Link>
+
         {/* {isLoggedIn ? (
           <button onClick={handleLogout}>Logout</button>
         ) : (
@@ -60,11 +52,20 @@ const Navbar = () => {
         )} */}
       </div>
 
-      <div className={`hidden sm:block mr-6 ${styles.menu}`}>
-        <button onClick={handleClick}>
-          {bars ? <img src={menu.src} /> : <img src={cross.src} />}
+      <div className={`hidden mr-6 ${styles.menu}`}>
+        <button onClick={() => {
+          setBars(!bars);
+          console.log(bars);
+        }} >
+          {bars ?
+            <img src={menu.src} />
+            :
+            <img src={cross.src} />
+
+          }
         </button>
       </div>
+
     </div>
   );
 };
