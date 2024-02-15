2eaa23d4d75c7fc6375fdc067bd88cdcc6d5c12c
Mayank Gupta <mayankgupta20231@gmail.com>
2024-02-15 19:06:56 +0530

    Update navbar component and styles for mobile responsiveness

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,7 +10,7 @@ import menu from '../assets/menu.svg'
 import cross from '../assets/cross2.svg'
 
 const Navbar = () => {
-  const [bars, setBars] = useState(false);
+  const [bars, setBars] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const auth = getAuth(app);
   const router = useRouter();
@@ -22,6 +22,8 @@ const Navbar = () => {
     return () => unsubscribe();
   }, []);
 
+  // console.log(bars)
+
   const handleLogout = async () => {
     try {
       await signOut(auth);
@@ -32,22 +34,35 @@ const Navbar = () => {
   };
   const handleLogoClick = async () => {
     try {
-     Router.push('/');
+      Router.push('/');
     } catch (error) {
       console.error('Error logging out:', error);
     }
   };
 
+  const handleClick = () => {
+    // Check if the viewport width is less than 640px using media query
+    const isMobile = window.matchMedia('(max-width: 640px)').matches;
+
+    if (isMobile) {
+      console.log('Button clicked on a mobile device (viewport width < 640px)');
+      setBars(!bars);
+      // Add your desired logic or additional code here
+    }
+  };
+
   return (
     <div style={{ color: 'rgb(102, 102, 102)' }} className={`h-full bg-white flex justify-between tracking-tight pl-4 pr-2 items-center font-medium  rounded-l font-inter ${styles.b1}`}>
-     <a href='/' onClick={handleLogoClick}>
-  <div className='flex text-black gap-4 items-center text-3xl'>
-    <img src={logo.src} alt='logo' className={`h-12 ${styles.logo}`} />
-  </div>
-</a>
+      <a href='/' onClick={handleLogoClick}>
+        <div className='flex text-black gap-4 items-center text-3xl'>
+          <img src={logo.src} alt='logo' className={`h-12 ${styles.logo}`} />
+        </div>
+      </a>
 
 
-      <div className={bars ? 'hidden' : `text-base flex  justify-around min-w-80 ${styles.nav}`}>
+      <div className={bars ? `${styles.navhide} sm:text-base sm:flex sm:justify-around sm:min-w-80  ` : `text-base flex justify-around min-w-80 ${styles.nav}`}
+        onClick={handleClick}
+      >
         <Link href="/">Features</Link>
         <Link href="/pricing">Pricing</Link>
         <Link href="/pricing">Contact</Link>
